15a83831226c6f623ece80c2c5d46959bc79847a
rostekus <rostyslavmosorov@gmail.com>
2022-02-24 00:31:20 +0100

    assert that the API KEY is given to run tests

--- tests/test_stops.py
+++ tests/test_stops.py
@@ -1,15 +1,16 @@
 import pytest
 import stops.stops as stops
+from stops.stops import API_KEY
 
-
+assert API_KEY != "YOUR_API_KEY"
 @pytest.mark.parametrize(
     "address, number, expected_result",
     [
         ("", "1332", (0, 0)),
         ("", "", (0, 0)),
         ("f435rfds", "fgegffd", (0, 0)),
-        # ("Piotrkowska pl. Niepodległości","0686",(51.7402595, 19.4626091)),
-        # ("Aleksandrowska Lechicka" "0022", (51.8040076, 19.3618336)),
+        ("Piotrkowska pl. Niepodległości","0686",(51.7402595, 19.4626091)),
+        ("Aleksandrowska Lechicka" "0022", (51.8040076, 19.3618336)),
     ],
 )
 def test_get_location(address, number, expected_result):
