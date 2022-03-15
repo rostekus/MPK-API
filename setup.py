2ab3bd9d19bfbe1bef5571b5750098ef101a92a5
rostekus <rostyslavmosorov@gmail.com>
2022-03-15 11:46:06 +0100

    added requirements install

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import find_packages
 from setuptools import setup
+import os
 
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 setup(
     name='MPK API',
     version='1.0.0',
@@ -9,6 +16,7 @@ setup(
     author_email='rmosorov@icloud.com',
     license='MIT License',
     url='https://github.com/rostekus/MPK-API',
+    install_requires=install_requires,
     packages=find_packages(where='src', exclude='tests*'),
     package_dir={'': 'src'}
 
